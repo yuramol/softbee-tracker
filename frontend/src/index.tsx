0a3ba2942e3bf891fbfe00761ff06c5650695834
Станіслав Бабич <themissingarchi@gmail.com>
2022-07-13 17:40:44 +0300

    feat: apollo client  setup  for easy work

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -11,11 +11,11 @@ const root = ReactDOM.createRoot(
 );
 
 root.render(
-  <React.StrictMode>
-    <ApolloProvider client={apolloClient}>
-      <App />
-    </ApolloProvider>
-  </React.StrictMode>
+	<React.StrictMode>
+		<ApolloProvider client={apolloClient}>
+			<App />
+		</ApolloProvider>
+	</React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
