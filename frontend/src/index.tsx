81f460d22de5508f3e86c57e1ce92ba6e08a72f9
Станіслав Бабич <themissingarchi@gmail.com>
2022-07-13 17:40:44 +0300

    feat: apollo client  setup  for easy work

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,25 +3,19 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import {
-	ApolloClient,
-	InMemoryCache,
-	ApolloProvider,
-	gql,
-} from "@apollo/client";
-import { ApolloProvider } from '@apollo/client';
-import { apolloClient } from './api';
+import { ApolloProvider } from "@apollo/client";
+import { apolloClient } from "./api";
 
 const root = ReactDOM.createRoot(
 	document.getElementById("root") as HTMLElement,
 );
 
 root.render(
-  <React.StrictMode>
-    <ApolloProvider client={apolloClient}>
-      <App />
-    </ApolloProvider>
-  </React.StrictMode>
+	<React.StrictMode>
+		<ApolloProvider client={apolloClient}>
+			<App />
+		</ApolloProvider>
+	</React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
