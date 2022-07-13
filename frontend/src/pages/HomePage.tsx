9ab0df2e59b3d931d92e65a6f17db9157f442ad0
Станіслав Бабич <themissingarchi@gmail.com>
2022-07-13 16:02:27 +0300

    feat: add routing

---
+++ frontend/src/pages/HomePage.tsx
@@ -0,0 +1,8 @@
+export const HomePage = () => {
+	return (
+		<div>
+			{" "}
+			<p>Home page</p>{" "}
+		</div>
+	);
+};
---
+++ frontend/src/pages/LoginPage.tsx
@@ -0,0 +1,8 @@
+export const LoginPage = () => {
+	return (
+		<div>
+			<p>Login page</p>
+			<input></input>{" "}
+		</div>
+	);
+};
