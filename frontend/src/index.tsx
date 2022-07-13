9d3566295f06bfb4138536bcfdcded1657bebc29
Станіслав Бабич <themissingarchi@gmail.com>
2022-07-13 16:51:21 +0300

    feat: add apollo client

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -8,7 +8,7 @@ import { apolloClient } from './api';
 import { theme } from './theme';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+	document.getElementById("root") as HTMLElement,
 );
 
 root.render(
