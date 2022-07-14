cc48eabe4432cd203c95ef5daf9caa28a520d60f
Станіслав Бабич <themissingarchi@gmail.com>
2022-07-14 16:17:15 +0300

    refactor: code style

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter} from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import AppRouter from './routes/Routes';
 
 function App() {
--- frontend/src/routes/Routes.tsx
+++ frontend/src/routes/Routes.tsx
@@ -2,13 +2,12 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { HomePage, LoginPage, ProfilePage } from '../pages';
 
-
 const AppRouter = () => (
-    <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
-    </Routes>
-  );
+  <Routes>
+    <Route path="/" element={<HomePage />} />
+    <Route path="/login" element={<LoginPage />} />
+    <Route path="/profile" element={<ProfilePage />} />
+  </Routes>
+);
 
 export default AppRouter;
