c0fd919c2ef901535c3ff8afe96e87500c3a7f84
AirFast <airfast.88@gmail.com>
2022-07-14 16:42:09 +0300

    refactor: code style

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,13 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { ApolloProvider } from "@apollo/client";
-import { apolloClient } from "./api";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { ApolloProvider } from '@apollo/client';
+import { apolloClient } from './api';
 
 const root = ReactDOM.createRoot(
-	document.getElementById("root") as HTMLElement,
+  document.getElementById('root') as HTMLElement
 );
 
 root.render(
