1d151480aa95677f85d3fb61292a1e59da6285e1
AirFast <airfast.88@gmail.com>
2022-07-14 16:42:09 +0300

    refactor: code style

--- frontend/src/api/apolloClient.ts
+++ frontend/src/api/apolloClient.ts
@@ -1,6 +1,6 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 export const apolloClient = new ApolloClient({
-	uri: process.env.REACT_APP_GRAPHQL_URI,
-	cache: new InMemoryCache(),
+  uri: process.env.REACT_APP_GRAPHQL_URI,
+  cache: new InMemoryCache(),
 });
