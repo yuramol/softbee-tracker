101b622e32307e276d93c3d3013d43afb625a5de
AirFast <airfast.88@gmail.com>
2022-07-14 16:42:09 +0300

    refactor: code style

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,21 +1,21 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { ApolloProvider } from "@apollo/client";
-import { apolloClient } from "./api";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { ApolloProvider } from '@apollo/client';
+import { apolloClient } from './api';
 
 const root = ReactDOM.createRoot(
-	document.getElementById("root") as HTMLElement,
+  document.getElementById('root') as HTMLElement
 );
 
 root.render(
-	<React.StrictMode>
-		<ApolloProvider client={apolloClient}>
-			<App />
-		</ApolloProvider>
-	</React.StrictMode>,
+  <React.StrictMode>
+    <ApolloProvider client={apolloClient}>
+      <App />
+    </ApolloProvider>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
