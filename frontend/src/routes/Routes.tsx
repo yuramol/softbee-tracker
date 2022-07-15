8f49a569180d9ff1414f39be88ed35e0d75d5b2e
AirFast <airfast.88@gmail.com>
2022-07-15 14:33:56 +0300

    refactor: import pages and routes

--- frontend/src/routes/Routes.tsx
+++ frontend/src/routes/Routes.tsx
@@ -2,12 +2,10 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { HomePage, LoginPage, ProfilePage } from '../pages';
 
-const AppRouter = () => (
+export const AppRouter = () => (
   <Routes>
     <Route path="/" element={<HomePage />} />
     <Route path="/login" element={<LoginPage />} />
     <Route path="/profile" element={<ProfilePage />} />
   </Routes>
 );
-
-export default AppRouter;
