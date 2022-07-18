8336b20b70d0ca47f9cc9f672a8180d0c391fe5b
madmaxen <sharkkizayk@gmail.com>
2022-07-18 11:47:30 +0300

    feat: create title day-track, add mui-icons and add custom button

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter } from 'react-router-dom';
 import AppRouter from './routes/Routes';
+import { DayViewTracker } from './components';
 
 function App() {
   return (
     <BrowserRouter>
       <AppRouter />
+      <DayViewTracker />
     </BrowserRouter>
   );
 }
---
+++ frontend/src/components/DayViewTracker/index.ts
@@ -0,0 +1,1 @@
+export { DayViewTracker } from './DayViewTracker';
