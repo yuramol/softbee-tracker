468d82850c47c2222c6e27f5829ad65f8d4eadd2
AirFast <airfast.88@gmail.com>
2022-07-18 18:33:49 +0300

    feat: implement user login logic

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,15 +1,61 @@
 import React from 'react';
-import { TextField, Button, Container, Stack, Typography } from '@mui/material';
+import { gql, useMutation } from '@apollo/client';
+import {
+  Container,
+  Grid,
+  Stack,
+  Typography,
+  TextField,
+  Button,
+} from '@mui/material';
 import { Formik } from 'formik';
 import * as yup from 'yup';
 
+type LoginValues = {
+  identifier: string;
+  password: string;
+};
+
+type SubmitActions = {
+  setSubmitting: (isSubmitting: boolean) => void;
+};
+
+const LOGIN = gql`
+  mutation Login($input: UsersPermissionsLoginInput!) {
+    login(input: $input) {
+      jwt
+      user {
+        id
+        username
+        email
+        role {
+          id
+          name
+          type
+        }
+      }
+    }
+  }
+`;
+
 export const LoginPage = () => {
-  // const handleSubmit = (e) => {
-  //   console.log(e);
-  // };
+  const [login, { data, loading, error }] = useMutation(LOGIN);
+
+  const handleLogin = async (
+    values: LoginValues,
+    { setSubmitting }: SubmitActions
+  ) => {
+    await login({ variables: { input: values } });
+
+    if (data) {
+      values.identifier = '';
+      values.password = '';
+      setSubmitting(false);
+    }
+  };
 
   const validationSchema = yup.object({
-    email: yup
+    identifier: yup
       .string()
       .email('Please enter a valid e-mail address')
       .required('Should not be empty'),
@@ -21,63 +67,71 @@ export const LoginPage = () => {
 
   return (
     <Container>
-      <Formik
-        initialValues={{ email: '', password: '' }}
-        validationSchema={validationSchema}
-        onSubmit={(values, { setSubmitting }) => {
-          setTimeout(() => {
-            console.log(values);
-            setSubmitting(false);
-          }, 400);
-        }}
-      >
-        {({
-          values,
-          errors,
-          handleChange,
-          handleBlur,
-          handleSubmit,
-          isSubmitting,
-        }) => (
-          <Stack
-            component={'form'}
-            spacing={3}
-            paddingY={'100px'}
-            maxWidth={500}
-            marginX={'auto'}
-            onSubmit={handleSubmit}
+      <Grid container justifyContent="center" alignItems="center">
+        <Grid item xs={10} sm={8} md={5}>
+          <Formik
+            initialValues={{ identifier: '', password: '' }}
+            validationSchema={validationSchema}
+            onSubmit={handleLogin}
           >
-            <Typography variant="h4" textAlign={'center'}>
-              Login
-            </Typography>
-            <TextField
-              variant="outlined"
-              label="Email"
-              type="email"
-              name="email"
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.email}
-              error={!!errors.email}
-              helperText={errors.email}
-            />
-            <TextField
-              variant="outlined"
-              label="Password"
-              type="password"
-              name="password"
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.password}
-              error={!!errors.password}
-              helperText={errors.password}
-            />
-            <Button variant="contained" type="submit" disabled={isSubmitting}>
-              Login
-            </Button>
-          </Stack>
-        )}
-      </Formik>
+            {({
+              values,
+              errors,
+              touched,
+              handleChange,
+              handleBlur,
+              handleSubmit,
+              isSubmitting,
+            }) => (
+              <Stack
+                spacing={4}
+                component="form"
+                paddingY="180px"
+                onSubmit={handleSubmit}
+              >
+                <Typography variant="h4" component="h1" align="center">
+                  Login
+                </Typography>
+                {!!error && (
+                  <Typography align="center" color="red" fontSize={12}>
+                    {error.message}
+                  </Typography>
+                )}
+                <TextField
+                  variant="outlined"
+                  label="Email"
+                  type="email"
+                  name="identifier"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.identifier}
+                  error={touched.identifier && !!errors.identifier}
+                  helperText={touched.identifier && errors.identifier}
+                />
+                <TextField
+                  variant="outlined"
+                  label="Password"
+                  type="password"
+                  name="password"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.password}
+                  error={touched.password && !!errors.password}
+                  helperText={touched.password && errors.password}
+                />
+                <Button
+                  variant="contained"
+                  type="submit"
+                  size="large"
+                  disabled={isSubmitting}
+                >
+                  {loading ? 'Loading...' : 'Login'}
+                </Button>
+              </Stack>
+            )}
+          </Formik>
+        </Grid>
+      </Grid>
     </Container>
   );
 };
