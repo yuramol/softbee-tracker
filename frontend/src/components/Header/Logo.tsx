73c511d2283ba54c1f0783891744f2618e1d39d7
Станіслав Бабич <themissingarchi@gmail.com>
2022-07-19 09:27:09 +0300

    feat:clicable logo ,approve semantics

--- frontend/src/components/Header/Logo.tsx
+++ frontend/src/components/Header/Logo.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import LogoMain from '../../assets/SoftBee_Logo.svg';
 
-export const Logo = () => <img alt="logo" src={LogoMain} width="100px" />;
+export const Logo = () => (
+  <Link to="/">
+    <img alt="logo" src={LogoMain} width="100px" />
+  </Link>
+);
