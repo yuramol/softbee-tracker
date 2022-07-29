de1ece88ea29a37d06ae4aef64f52f6da6b6d06c
AirFast <airfast.88@gmail.com>
2022-07-29 15:39:07 +0300

    feat: journey tracker history

--- frontend/src/components/TrackerDayView/DayTabs.tsx
+++ frontend/src/components/TrackerDayView/DayTabs.tsx
@@ -1,19 +1,29 @@
-import React, { FC } from 'react';
+import React from 'react';
 import { Grid, Tab, Tabs, Typography } from '@mui/material';
+import { isAfter, isFuture, startOfDay, startOfMonth } from 'date-fns';
 
 import { PanelTab } from './PanelTab';
 import { useCurrentWeek } from '../../hooks';
 import { getTotalTime } from '../../helpers';
 import { TrackerEntity } from '../../types/GraphqlTypes';
+import { Role } from '../../constants';
+import { useAuth } from '../../AuthProvider';
 
 type Props = {
+  currentDate: Date;
+  dataTabs: TrackerEntity[] | undefined;
   tabsValue: number;
   setTabsValue: (newValue: number) => void;
-  dataTabs: TrackerEntity[] | undefined;
 };
 
-export const DayTabs: FC<Props> = ({ setTabsValue, tabsValue, dataTabs }) => {
-  const { days } = useCurrentWeek(new Date());
+export const DayTabs: React.FC<Props> = ({
+  currentDate,
+  dataTabs,
+  tabsValue,
+  setTabsValue,
+}) => {
+  const { user } = useAuth();
+  const { days } = useCurrentWeek(currentDate);
   const totalTime = getTotalTime(dataTabs);
 
   return (
@@ -39,6 +49,14 @@ export const DayTabs: FC<Props> = ({ setTabsValue, tabsValue, dataTabs }) => {
                     )}
                   </Typography>
                 </>
+              }
+              disabled={
+                (isAfter(
+                  startOfMonth(currentDate),
+                  startOfDay(new Date(fullDate))
+                ) &&
+                  user.role.type === Role.Employee) ||
+                isFuture(new Date(fullDate))
               }
             />
           ))}
--- frontend/src/components/TrackerDayView/TrackerDayView.tsx
+++ frontend/src/components/TrackerDayView/TrackerDayView.tsx
@@ -1,7 +1,16 @@
 import React, { createContext, useState } from 'react';
 
 import { useQuery, useMutation } from '@apollo/client';
-import { format } from 'date-fns';
+import {
+  addDays,
+  format,
+  isAfter,
+  isEqual,
+  isFuture,
+  startOfDay,
+  startOfMonth,
+  subDays,
+} from 'date-fns';
 import { Typography, Button, Grid } from '@mui/material';
 import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore';
 import NavigateNextIcon from '@mui/icons-material/NavigateNext';
@@ -11,13 +20,14 @@ import { useCurrentWeek } from '../../hooks';
 import { DayTabs } from './DayTabs';
 import { TrackerAddNewEntry } from '../TrackerAddNewEntry';
 import {
-  Maybe,
-  TrackerEntityResponseCollection,
-} from '../../types/GraphqlTypes';
-import {
   TRECKERS_BY_USER_ID_QUERY,
   UPDATE_TRACKER_BY_ID_MUTATION,
 } from '../../api';
+import { Role } from '../../constants';
+import {
+  Maybe,
+  TrackerEntityResponseCollection,
+} from '../../types/GraphqlTypes';
 
 export type TimeContextType = {
   onUpdateTime: (time: Date, id: Maybe<string> | undefined) => void;
@@ -29,8 +39,9 @@ export const TimeContext = createContext<TimeContextType>(
 
 export const TrackerDayView = () => {
   const { user } = useAuth();
-  const { weekEnd, weekStart, days, currentDay } = useCurrentWeek(new Date());
-  const [tabsValue, setTabsValue] = useState(+currentDay - 1);
+  const [currentDate, setCurrentDate] = useState(new Date());
+  const { weekStart, weekEnd, days, currentDay } = useCurrentWeek(currentDate);
+  const [tabsValue, setTabsValue] = useState(currentDay);
 
   const { data, refetch } = useQuery<{
     trackers: TrackerEntityResponseCollection;
@@ -46,32 +57,86 @@ export const TrackerDayView = () => {
     });
   };
 
+  const handleCurrentDate = () => {
+    setCurrentDate(new Date());
+    setTabsValue(+format(new Date(), 'i') - 1);
+  };
+
+  const handlePrevDate = () => {
+    if (tabsValue === 0) {
+      setTabsValue(6);
+      setCurrentDate(subDays(new Date(weekStart), 1));
+      return;
+    }
+
+    setTabsValue(tabsValue - 1);
+  };
+
+  const handleNextDate = () => {
+    if (tabsValue === 6) {
+      setTabsValue(0);
+      setCurrentDate(addDays(new Date(weekEnd), 1));
+      return;
+    }
+
+    setTabsValue(tabsValue + 1);
+  };
+
+  const isToday = isEqual(
+    startOfDay(new Date()),
+    startOfDay(new Date(days[tabsValue].fullDate))
+  );
+
+  const isStartEditForEmployee =
+    isAfter(
+      startOfMonth(currentDate),
+      subDays(startOfDay(new Date(days[tabsValue].fullDate)), 1)
+    ) && user.role.type === Role.Employee;
+
+  const isEndEdit = isFuture(addDays(new Date(days[tabsValue].fullDate), 1));
+
   return (
     <TimeContext.Provider value={{ onUpdateTime } as TimeContextType}>
-      <Grid container gap={2} marginTop={6}>
-        <Button
-          variant="outlined"
-          disabled={tabsValue === 0}
-          onClick={() => setTabsValue(tabsValue - 1)}
-        >
-          <NavigateBeforeIcon />
-        </Button>
-        <Button
-          variant="outlined"
-          disabled={tabsValue === 6}
-          onClick={() => setTabsValue(tabsValue + 1)}
-        >
-          <NavigateNextIcon />
-        </Button>
-        <Typography variant="h6" marginLeft={2}>
-          {days[tabsValue].day}, {days[tabsValue].date}
-        </Typography>
+      <Grid
+        container
+        gap={2}
+        marginTop={6}
+        justifyContent="space-between"
+        alignItems="center"
+      >
+        <Grid display="flex" gap={2}>
+          <Button
+            variant="outlined"
+            disabled={isStartEditForEmployee}
+            onClick={handlePrevDate}
+          >
+            <NavigateBeforeIcon />
+          </Button>
+          <Button
+            variant="outlined"
+            disabled={isEndEdit}
+            onClick={handleNextDate}
+          >
+            <NavigateNextIcon />
+          </Button>
+          <Typography variant="h6" marginLeft={2}>
+            {days[tabsValue].day}, {days[tabsValue].date}
+          </Typography>
+        </Grid>
+        {!isToday && (
+          <Grid>
+            <Button variant="contained" onClick={handleCurrentDate}>
+              Today
+            </Button>
+          </Grid>
+        )}
       </Grid>
       <Grid marginY={4}>
         <DayTabs
+          currentDate={currentDate}
           dataTabs={data?.trackers.data}
-          setTabsValue={setTabsValue}
           tabsValue={tabsValue}
+          setTabsValue={setTabsValue}
         />
       </Grid>
       <TrackerAddNewEntry />
