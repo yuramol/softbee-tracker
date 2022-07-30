9c2d318f6ddb05e70ba432dc71ad6f4210a6db03
vitalekp <80340001+vitalekp@users.noreply.github.com>
2022-07-30 14:36:14 +0300

    Feat/st 20 tracker calendar component (#22)
    * feat: add calendar picker
    
    * create tracker calendar component
    
    * Hide badge if not current month
    
    * refactor: refactoring of all code
    
    * fix: BADGE positioning bug fixes
    
    Co-authored-by: yuramoldev <yuramol86@gmail.com>

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-
 import { Typography } from '@mui/material';
-import { MainWrapper, TrackerDayView } from '../components';
+
+import { MainWrapper, TrackerCalendar, TrackerDayView } from '../components';
 
 const HomePage = () => {
   return (
-    <MainWrapper sidebar={<p>Width right sidebar</p>}>
-      <Typography variant="h1">Tracker</Typography>
+    <MainWrapper sidebar={<TrackerCalendar />}>
+      <Typography variant='h1'>Tracker</Typography>
       <TrackerDayView />
     </MainWrapper>
   );
