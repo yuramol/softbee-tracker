f9f00915d34adcc303d805db2e059d526500d28c
AirFast <airfast.88@gmail.com>
2022-08-01 14:48:57 +0300

    feat: add time inspector to the sidebar

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -2,11 +2,19 @@ import React from 'react';
 import { Typography } from '@mui/material';
 
 import { MainWrapper, TrackerCalendar, TrackerDayView } from '../components';
+import { TimeSection } from 'components/TimeInspector/TimeSection';
 
 const HomePage = () => {
   return (
-    <MainWrapper sidebar={<TrackerCalendar />}>
-      <Typography variant='h1'>Tracker</Typography>
+    <MainWrapper
+      sidebar={
+        <>
+          <TimeSection />
+          <TrackerCalendar />
+        </>
+      }
+    >
+      <Typography variant="h1">Tracker</Typography>
       <TrackerDayView />
     </MainWrapper>
   );
