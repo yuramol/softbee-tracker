d4b74e904b193e81f31bead884fcb1cd968f8216
vitalekp <vitalekpushkarov@gmail.com>
2022-08-02 09:54:39 +0300

    fix: Formik values output fixes

--- frontend/src/components/TrackerAddNewEntry/TrackerAddNewEntry.tsx
+++ frontend/src/components/TrackerAddNewEntry/TrackerAddNewEntry.tsx
@@ -7,17 +7,11 @@ import {
   Tooltip,
   Grid,
 } from '@mui/material';
-import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
 import AddIcon from '@mui/icons-material/Add';
-import enGb from 'date-fns/locale/en-GB';
-
 import { FormikProps, Formik } from 'formik';
-import * as Yup from 'yup';
 
 import { ModalSelect } from '../../legos/ModalSelect';
-import { useCurrentWeek } from '../../hooks';
+import { TrackerDate } from 'legos/TrackerDate';
 
 const modalStyle = {
   width: 600,
@@ -28,6 +22,7 @@ const modalStyle = {
 };
 
 interface TimeEntryValues {
+  date: string;
   time: string;
   description: string;
   project: string;
@@ -35,18 +30,15 @@ interface TimeEntryValues {
 
 export const TrackerAddNewEntry = () => {
   const [isOpenModal, setIsOpenModal] = useState(false);
-  const { days, currentDay } = useCurrentWeek(new Date());
-  const [time, setTime] = useState('');
-  const [value, setValue] = React.useState<Date | null>(new Date());
 
   // TODO Add projects from backend
   const itemSelectProject = [{ label: 'softbee' }, { label: 'demigos' }];
 
   return (
     <>
-      <Tooltip title="Add New Entry">
+      <Tooltip title='Add New Entry'>
         <Button
-          variant="contained"
+          variant='contained'
           onClick={() => setIsOpenModal(!isOpenModal)}
         >
           <AddIcon />
@@ -58,74 +50,60 @@ export const TrackerAddNewEntry = () => {
         onClose={() => setIsOpenModal(!isOpenModal)}
       >
         <Formik
-          initialValues={{ time: '', description: '', project: '' }}
+          initialValues={{ date: '', time: '', description: '', project: '' }}
           onSubmit={(values) => {
             console.log('====', values);
           }}
         >
           {(props: FormikProps<TimeEntryValues>) => (
             <form onSubmit={props.handleSubmit}>
-              <Grid container justifyContent="center" alignItems="center">
+              <Grid container justifyContent='center' alignItems='center'>
                 <Grid sx={modalStyle}>
-                  <Typography variant="h6" marginBottom="10px">
+                  <Typography variant='h6' marginBottom='10px'>
                     {`New time entry`}
                   </Typography>
                   <Grid
                     item
-                    marginTop="20px"
-                    display="flex"
-                    justifyContent="space-between"
+                    marginTop='20px'
+                    display='flex'
+                    justifyContent='space-between'
                   >
-                    <LocalizationProvider
-                      adapterLocale={enGb}
-                      dateAdapter={AdapterDateFns}
-                    >
-                      <DesktopDatePicker
-                        disablePast
-                        value={value}
-                        onChange={(newValue) => {
-                          setValue(newValue);
-                        }}
-                        renderInput={(params) => (
-                          <TextField {...params} fullWidth />
-                        )}
-                      />
-                    </LocalizationProvider>
+                    <TrackerDate />
                     <TextField
-                      id="time"
-                      name="time"
-                      type="time"
-                      variant="outlined"
+                      id='time'
+                      name='time'
+                      type='time'
+                      variant='outlined'
                       sx={{ width: '60%', marginLeft: 2 }}
-                      onChange={(e) => setTime(e.target.value || '00:00')}
+                      onChange={props.handleChange}
                     />
                   </Grid>
                   <ModalSelect
-                    id="project"
-                    name="project"
-                    label="Project"
+                    id='project'
+                    name='project'
+                    label='Project'
                     items={itemSelectProject}
                   />
                   <TextField
-                    id="description"
-                    name="description"
+                    id='description'
+                    name='description'
                     fullWidth
                     multiline
                     rows={4}
-                    placeholder="Description"
+                    placeholder='Description'
                     sx={{ marginTop: 2 }}
                     onChange={props.handleChange}
                   />
-                  <Grid marginTop="20px">
+                  <Grid marginTop='20px'>
                     <Button
                       sx={{ mr: '10px' }}
-                      variant="contained"
-                      type="submit"
+                      variant='contained'
+                      type='submit'
                     >
                       Save Time
                     </Button>
                     <Button
-                      variant="outlined"
+                      variant='outlined'
                       onClick={() => setIsOpenModal(!isOpenModal)}
                     >
                       Cancel
