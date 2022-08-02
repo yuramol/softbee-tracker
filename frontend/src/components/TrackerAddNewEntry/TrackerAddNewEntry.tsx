5ace7f8ed623fae31552c6388f0cd1f6c027aea7
yuramoldev <yuramol86@gmail.com>
2022-08-02 10:25:52 +0300

    Use FormikContext

--- frontend/src/components/TrackerAddNewEntry/TrackerAddNewEntry.tsx
+++ frontend/src/components/TrackerAddNewEntry/TrackerAddNewEntry.tsx
@@ -8,7 +8,7 @@ import {
   Grid,
 } from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
-import { FormikProps, Formik } from 'formik';
+import { useFormik, FormikContext } from 'formik';
 
 import { ModalSelect } from '../../legos/ModalSelect';
 import { TrackerDate } from 'legos/TrackerDate';
@@ -34,11 +34,25 @@ export const TrackerAddNewEntry = () => {
   // TODO Add projects from backend
   const itemSelectProject = [{ label: 'softbee' }, { label: 'demigos' }];
 
+  const formik = useFormik<TimeEntryValues>({
+    initialValues: {
+      date: '',
+      time: '',
+      description: '',
+      project: '',
+    },
+    onSubmit: (values) => {
+      console.log('===', values);
+    },
+  });
+
+  const { handleChange, handleSubmit } = formik;
+
   return (
     <>
-      <Tooltip title='Add New Entry'>
+      <Tooltip title="Add New Entry">
         <Button
-          variant='contained'
+          variant="contained"
           onClick={() => setIsOpenModal(!isOpenModal)}
         >
           <AddIcon />
@@ -49,71 +63,60 @@ export const TrackerAddNewEntry = () => {
         closeAfterTransition
         onClose={() => setIsOpenModal(!isOpenModal)}
       >
-        <Formik
-          initialValues={{ date: '', time: '', description: '', project: '' }}
-          onSubmit={(values) => {
-            console.log('====', values);
-          }}
-        >
-          {(props: FormikProps<TimeEntryValues>) => (
-            <form onSubmit={props.handleSubmit}>
-              <Grid container justifyContent='center' alignItems='center'>
-                <Grid sx={modalStyle}>
-                  <Typography variant='h6' marginBottom='10px'>
-                    {`New time entry`}
-                  </Typography>
-                  <Grid
-                    item
-                    marginTop='20px'
-                    display='flex'
-                    justifyContent='space-between'
-                  >
-                    <TrackerDate />
-                    <TextField
-                      id='time'
-                      name='time'
-                      type='time'
-                      variant='outlined'
-                      sx={{ width: '60%', marginLeft: 2 }}
-                      onChange={props.handleChange}
-                    />
-                  </Grid>
-                  <ModalSelect
-                    id='project'
-                    name='project'
-                    label='Project'
-                    items={itemSelectProject}
-                  />
+        <FormikContext.Provider value={formik}>
+          <form onSubmit={handleSubmit}>
+            <Grid container justifyContent="center" alignItems="center">
+              <Grid sx={modalStyle}>
+                <Typography variant="h6" marginBottom="10px">
+                  {`New time entry`}
+                </Typography>
+                <Grid
+                  item
+                  marginTop="20px"
+                  display="flex"
+                  justifyContent="space-between"
+                >
+                  <TrackerDate />
                   <TextField
-                    id='description'
-                    name='description'
-                    fullWidth
-                    multiline
-                    rows={4}
-                    placeholder='Description'
-                    sx={{ marginTop: 2 }}
-                    onChange={props.handleChange}
+                    id="time"
+                    name="time"
+                    type="time"
+                    variant="outlined"
+                    sx={{ width: '60%', marginLeft: 2 }}
+                    onChange={handleChange}
                   />
-                  <Grid marginTop='20px'>
-                    <Button
-                      sx={{ mr: '10px' }}
-                      variant='contained'
-                      type='submit'
-                    >
-                      Save Time
-                    </Button>
-                    <Button
-                      variant='outlined'
-                      onClick={() => setIsOpenModal(!isOpenModal)}
-                    >
-                      Cancel
-                    </Button>
-                  </Grid>
+                </Grid>
+                <ModalSelect
+                  id="project"
+                  name="project"
+                  label="Project"
+                  items={itemSelectProject}
+                />
+                <TextField
+                  id="description"
+                  name="description"
+                  fullWidth
+                  multiline
+                  rows={4}
+                  placeholder="Description"
+                  sx={{ marginTop: 2 }}
+                  onChange={handleChange}
+                />
+                <Grid marginTop="20px">
+                  <Button sx={{ mr: '10px' }} variant="contained" type="submit">
+                    Save Time
+                  </Button>
+                  <Button
+                    variant="outlined"
+                    onClick={() => setIsOpenModal(!isOpenModal)}
+                  >
+                    Cancel
+                  </Button>
                 </Grid>
               </Grid>
-            </form>
-          )}
-        </Formik>
+            </Grid>
+          </form>
+        </FormikContext.Provider>
       </Modal>
     </>
   );
