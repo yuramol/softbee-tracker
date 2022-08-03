41b3befceca938619ffd3158e0110a4c96c45c53
AirFast <airfast.88@gmail.com>
2022-08-03 12:20:49 +0300

    refactor: projects trackers query

--- frontend/src/api/graphql/query/projectsTrackersByUserId.ts
+++ frontend/src/api/graphql/query/projectsTrackersByUserId.ts
@@ -2,7 +2,7 @@ import { gql } from '@apollo/client';
 
 export const PROJECTS_TRECKERS_BY_USER_ID_QUERY = gql`
   query ProjectsTrackersByUserId($userId: ID!, $filterByDate: [Date]) {
-    projects {
+    projects(filters: { users: { id: { eq: $userId } } }) {
       data {
         id
         attributes {
