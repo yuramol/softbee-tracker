43923833e00531577c145755420e10be2d615329
Andrew Petryk <AirFast.88@gmail.com>
2022-08-04 15:25:39 +0300

    Feat: Edit, delete time tracker [ST-43] (#31)
    * feat: journey tracker history
    
    * feat: journey tracker history
    
    * refactor: time inspector
    
    * refactor: time inspector
    
    * feat: delete tracker by id
    
    * feat: journey tracker history
    
    * refactor: time inspector
    
    * feat: delete tracker by id
    
    * chore: update yarn.lock
    
    * chore: update yarn.lock
    
    * feat: add time inspector to the sidebar
    
    * feat: update buttons design in the tracker
    
    * feat: journey tracker history
    
    * refactor: time inspector
    
    * feat: delete tracker by id
    
    * feat: journey tracker history
    
    * feat: delete tracker by id
    
    * chore: update yarn.lock
    
    * chore: update yarn.lock
    
    * feat: add time inspector to the sidebar
    
    * feat: update buttons design in the tracker
    
    * refactor: correct use of grid component
    
    * refactor: using abbreviated prop names
    
    * refactor: border position between tracker elements

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -2,10 +2,18 @@ import React from 'react';
 import { Typography } from '@mui/material';
 
 import { MainWrapper, TrackerCalendar, TrackerDayView } from '../components';
+import { TimeSection } from 'components/TimeInspector/TimeSection';
 
 const HomePage = () => {
   return (
-    <MainWrapper sidebar={<TrackerCalendar />}>
+    <MainWrapper
+      sidebar={
+        <>
+          <TimeSection />
+          <TrackerCalendar />
+        </>
+      }
+    >
       <Typography variant="h1">Tracker</Typography>
       <TrackerDayView />
     </MainWrapper>
