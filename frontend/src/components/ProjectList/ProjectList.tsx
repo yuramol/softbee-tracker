04e4345337c208c44fc55436cfc8501ff96ccb69
yuramoldev <yuramol86@gmail.com>
2022-08-08 12:23:32 +0300

    refactor: project list

--- frontend/src/components/ProjectList/ProjectList.tsx
+++ frontend/src/components/ProjectList/ProjectList.tsx
@@ -1,7 +1,6 @@
 import React, { Fragment } from 'react';
-import { Avatar, Grid, IconButton, Link, Typography } from '@mui/material';
+import { Avatar, Grid, IconButton, Link } from '@mui/material';
 
-import { Box } from '@mui/system';
 import { UniversalAvatar, Icon } from 'legos';
 
 //TODO add projects info and info about PR
@@ -31,51 +30,45 @@ const projects = [
 ];
 
 export const ProjectList = () => (
-  <>
-    <Typography sx={{ mt: 3 }} variant="h5">
-      Projects
-    </Typography>
-    <Grid
-      sx={{ mt: 2 }}
-      container
-      spacing={2}
-      justifyContent="space-between"
-      alignItems="flex-start"
-    >
-      {projects.map((project) => (
-        <Fragment key={project.id}>
-          <Grid item xs={4} container alignItems="center">
-            <Avatar sx={{ backgroundColor: 'common.lightBackground', mr: 3 }}>
-              {project.type == 'paid' ? (
-                <Icon icon="paidOutlined" color="blue" />
-              ) : (
-                <Icon icon="moneyOff" color="red" />
-              )}
-            </Avatar>
-            <Box>
-              <Link href="*">{project.projectName}</Link>
-              <Typography fontSize="10px">{project.timeLine}</Typography>
-            </Box>
-          </Grid>
-          <Grid container gap={2} item xs={4} alignItems="center">
-            <UniversalAvatar
-              avatar={project.projectManagerAvatar}
-              name={project.projectManager}
-            />
-            <Link href="*" underline="none">
-              {project.projectManager}
-            </Link>
-          </Grid>
-          <Grid item container xs={2}>
-            <IconButton aria-label="edit">
-              <Icon icon="editOutlined" />
-            </IconButton>
-            <IconButton aria-label="archive">
-              <Icon icon="archiveOutlined" />
-            </IconButton>
-          </Grid>
-        </Fragment>
-      ))}
-    </Grid>
-  </>
+  <Grid
+    container
+    spacing={2}
+    marginTop={2}
+    justifyContent="space-between"
+    alignItems="flex-start"
+  >
+    {projects.map((project) => (
+      <Fragment key={project.id}>
+        <Grid item xs={4} container alignItems="center">
+          <Avatar>
+            {project.type == 'paid' ? (
+              <Icon icon="paidOutlined" color="blue" />
+            ) : (
+              <Icon icon="moneyOff" color="red" />
+            )}
+          </Avatar>
+          <Link href="*" sx={{ ml: 3 }}>
+            {project.projectName}
+          </Link>
+        </Grid>
+        <Grid container gap={2} item xs={4} alignItems="center">
+          <UniversalAvatar
+            avatar={project.projectManagerAvatar}
+            name={project.projectManager}
+          />
+          <Link href="*" underline="none">
+            {project.projectManager}
+          </Link>
+        </Grid>
+        <Grid item container xs={2}>
+          <IconButton aria-label="edit">
+            <Icon icon="editOutlined" />
+          </IconButton>
+          <IconButton aria-label="archive">
+            <Icon icon="archiveOutlined" />
+          </IconButton>
+        </Grid>
+      </Fragment>
+    ))}
+  </Grid>
 );
