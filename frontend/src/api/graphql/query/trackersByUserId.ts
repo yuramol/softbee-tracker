29645f14b79d9d3112d9cb9aa612f944aab115c3
AirFast <airfast.88@gmail.com>
2022-08-10 14:24:26 +0300

    feat: implement logic to useNormalizedTrackers hook

--- frontend/src/api/graphql/query/trackersByUserId.ts
+++ frontend/src/api/graphql/query/trackersByUserId.ts
@@ -4,6 +4,7 @@ export const TRECKERS_BY_USER_ID_QUERY = gql`
   query TrackersByUserId($userId: ID!, $period: [Date]) {
     trackers(
       filters: { user: { id: { eq: $userId } }, date: { between: $period } }
+      sort: "date"
     ) {
       data {
         id
