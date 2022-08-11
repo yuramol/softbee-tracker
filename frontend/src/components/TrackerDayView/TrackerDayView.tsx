52e3f318cf76ac856d08912d5ccae2f510710e60
AirFast <airfast.88@gmail.com>
2022-08-11 15:40:42 +0300

    refactor: update from front

--- frontend/src/components/TrackerDayView/TrackerDayView.tsx
+++ frontend/src/components/TrackerDayView/TrackerDayView.tsx
@@ -18,7 +18,7 @@ import NavigateNextIcon from '@mui/icons-material/NavigateNext';
 import { DayTabs } from './DayTabs';
 import { TrackerAddNewEntry } from '../TrackerAddNewEntry';
 import { useAuth } from 'AuthProvider';
-import { useCurrentWeek, useNormalizedTrackers } from 'hooks';
+import { useCurrentWeek } from 'hooks';
 import {
   TRECKERS_BY_USER_ID_QUERY,
   UPDATE_TRACKER_BY_ID_MUTATION,
@@ -57,11 +57,6 @@ export const TrackerDayView = ({ selectedDay }: TrackerDayViewProps) => {
     setCurrentWeekDay(selectedDay);
     setTabsValue(currentDay);
   }, [selectedDay]);
-
-  useNormalizedTrackers(user.id, ['2022-08-01', '2022-08-31']);
-  const trackers = useNormalizedTrackers(user.id, ['2022-08-01', '2022-08-31']);
-
-  console.log(trackers);
 
   const { data, refetch } = useQuery<{
     trackers: TrackerEntityResponseCollection;
