f14ac9f4bb49405e2582079795f32797a49d1efd
AirFast <airfast.88@gmail.com>
2022-08-11 13:47:28 +0300

    refactor: add helper functions

--- frontend/src/helpers/index.ts
+++ frontend/src/helpers/index.ts
@@ -1,2 +1,4 @@
 export { parseTrackerTime } from './parseTrackerTime';
 export { getTotalTime } from './getTotalTime';
+export { getMinutes } from './getMinutes';
+export { getHours } from './getHours';
