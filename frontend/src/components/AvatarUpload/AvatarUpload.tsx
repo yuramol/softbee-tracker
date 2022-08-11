5792638f13f564b76d8a3a60a4fdb2c68ba34541
dumik <dumik.alive@gmail.com>
2022-08-11 16:45:18 +0300

    Remove the redundant fragment

--- frontend/src/components/AvatarUpload/AvatarUpload.tsx
+++ frontend/src/components/AvatarUpload/AvatarUpload.tsx
@@ -12,14 +12,12 @@ type AvatarUploadProps = {
 export const AvatarUpload = ({ avatar, name, onChange }: AvatarUploadProps) => {
   const path = avatar ? `https://dev.strapi.track.softbee.io${avatar}` : '';
   return (
-    <>
-      <Box display="flex" flexDirection="column" alignItems="center" gap={2}>
-        <Avatar width={180} height={180} name={name} avatar={path} />
-        <Button component="label" color="primary" aria-label="upload picture">
-          <Typography fontSize={12}>Upload new photo</Typography>
-          <input hidden accept="image/*" type="file" onChange={onChange} />
-        </Button>
-      </Box>
-    </>
+    <Box display="flex" flexDirection="column" alignItems="center" gap={2}>
+      <Avatar width={180} height={180} name={name} avatar={path} />
+      <Button component="label" color="primary" aria-label="upload picture">
+        <Typography fontSize={12}>Upload new photo</Typography>
+        <input hidden accept="image/*" type="file" onChange={onChange} />
+      </Button>
+    </Box>
   );
 };
