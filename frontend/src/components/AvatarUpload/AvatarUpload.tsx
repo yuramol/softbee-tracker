5955e278c4ffd1d7dd7e619a5614dad83e2259c2
Oleg Duma <dumadesign1@gmail.com>
2022-08-11 17:31:30 +0300

    St 70 delete avatar (#45)
    * Implement removing the old avatar
    
    * Fix path for user avatar
    
    * Remove comment
    
    * Remove the redundant fragment

--- frontend/src/components/AvatarUpload/AvatarUpload.tsx
+++ frontend/src/components/AvatarUpload/AvatarUpload.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { ChangeEvent } from 'react';
 
 import { Typography, Box, Button } from '@mui/material';
 import { Avatar } from 'legos';
@@ -6,29 +6,18 @@ import { Avatar } from 'legos';
 type AvatarUploadProps = {
   avatar: Blob | MediaSource;
   name: string;
-  isLocalPath?: boolean;
-  onChange: (data: any) => void;
+  onChange: (data: ChangeEvent) => void;
 };
 
-export const AvatarUpload = ({
-  avatar,
-  name,
-  onChange,
-  isLocalPath,
-}: AvatarUploadProps) => {
-  const path = isLocalPath
-    ? URL.createObjectURL(avatar)
-    : `https://dev.strapi.track.softbee.io${avatar}`;
-
+export const AvatarUpload = ({ avatar, name, onChange }: AvatarUploadProps) => {
+  const path = avatar ? `https://dev.strapi.track.softbee.io${avatar}` : '';
   return (
-    <>
-      <Box display="flex" flexDirection="column" alignItems="center" gap={2}>
-        <Avatar width={180} height={180} name={name} avatar={path} />
-        <Button component="label" color="primary" aria-label="upload picture">
-          <Typography fontSize={12}>Upload new photo</Typography>
-          <input hidden accept="image/*" type="file" onChange={onChange} />
-        </Button>
-      </Box>
-    </>
+    <Box display="flex" flexDirection="column" alignItems="center" gap={2}>
+      <Avatar width={180} height={180} name={name} avatar={path} />
+      <Button component="label" color="primary" aria-label="upload picture">
+        <Typography fontSize={12}>Upload new photo</Typography>
+        <input hidden accept="image/*" type="file" onChange={onChange} />
+      </Button>
+    </Box>
   );
 };
