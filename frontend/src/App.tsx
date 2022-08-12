34a9499a83d7110d5cc7e397d98b0cc6a25e48a8
yuramoldev <yuramol86@gmail.com>
2022-08-12 15:25:08 +0300

    fix: refactor layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,29 +1,10 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import GlobalStyles from '@mui/material/GlobalStyles';
+import { GlobalStyles } from '@mui/material';
 
 import { AuthProvider } from './AuthProvider';
 import { AppRouter } from './routes';
-
-const globalStyles = `{
-  html: {
-    '-webkit-font-smoothing': 'antialiased',
-    '-moz-osx-font-smoothing': 'grayscale',
-    height: '100%',
-    width: '100%',
-  },
-  '*, *::before, *::after': {
-    boxSizing: 'inherit',
-  },
-  body: {
-    height: '100%',
-    width: '100%',
-  },
-  '#root': {
-    height: '100%',
-    width: '100%',
-  },
-}`;
+import { globalStyles } from 'theme/globalStyles';
 
 export const App = () => {
   return (
