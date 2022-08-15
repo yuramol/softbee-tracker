d02d7b3b5b67ad2fbf8a84b21ad72dcd63aab60a
Andrew Petryk <AirFast.88@gmail.com>
2022-08-15 18:46:57 +0300

    Feat: Create hook useNormalizedTrackers [ST-69] (#44)
    * feat: add hook function useNormalizedTrackers
    
    * feat: implement logic to useNormalizedTrackers hook
    
    * refactor: add helper functions
    
    * feat: add hook function useNormalizedTrackers
    
    * feat: implement logic to useNormalizedTrackers hook
    
    * refactor: add helper functions
    
    * refactor: update from front
    
    * St 70 delete avatar (#45)
    
    * Implement removing the old avatar
    
    * Fix path for user avatar
    
    * Remove comment
    
    * Remove the redundant fragment
    
    * Implement notification 
    
    * Implement notification
    
    * Fix show notification on save user
    
    * Change duration showing notifications
    
    * Feat: Login page improvements [ST-38] (#49)
    
    * feat: improve AuthProvider
    * refactor: use useFormik
    * refactor: fetch user data after login
    * fix: redirect to login page if user not auth
    
    * refactor: change reduce to forEach
    
    * feat: add hook function useNormalizedTrackers
    
    * feat: implement logic to useNormalizedTrackers hook
    
    * refactor: add helper functions
    
    * refactor: update from front
    
    * refactor: change reduce to forEach
    
    * refactor: graphql query argument improvements
    
    * St 67 project list fix (#47)
    
    * feat:replace grid to stack
    
    * feat: add multiple select  & search , fix enother elements
    
    * feat:add side bar component
    
    * St 64 profile page improvements (#50)
    
    * fix:improve page components
    
    * fix: Refactor Avatar layout
    
    * feat:avatar fix
    
    * fix:avatar required props
    
    Co-authored-by: yuramoldev <yuramol86@gmail.com>
    
    * feat: add tooltip text (#51)
    
    * fix: projectList props
    
    * layout: Header container layout
    
    * feat: add hook function useNormalizedTrackers
    
    * feat: implement logic to useNormalizedTrackers hook
    
    * refactor: add helper functions
    
    * refactor: update from front
    
    * refactor: change reduce to forEach
    
    * feat: add hook function useNormalizedTrackers
    
    * feat: implement logic to useNormalizedTrackers hook
    
    * refactor: add helper functions
    
    * refactor: add helper functions
    
    * refactor: update from front
    
    * refactor: graphql query argument improvements
    
    * feat: add limit (-1) to the TrackersByUserId query
    
    Co-authored-by: Oleg Duma <dumadesign1@gmail.com>
    Co-authored-by: Stanislav Babuch <stasbs1997@gmail.com>
    Co-authored-by: yuramoldev <yuramol86@gmail.com>

--- frontend/src/hooks/index.ts
+++ frontend/src/hooks/index.ts
@@ -1,4 +1,6 @@
 export { useCurrentWeek } from './useCurrentWeek';
 export { useLocalStorage } from './useLocalStorage';
 export { useUsersPermissionsUser } from './useUsersPermissionsUser';
+export { useNormalizedTrackers } from './useNormalizedTrackers';
+export { useNotification } from './useNotification';
 export { useDebounce } from './useDebounce';
