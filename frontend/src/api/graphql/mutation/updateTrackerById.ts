97cf96361943859fb937dd9992840e46dd81756c
Mikhail Yastremsky <maukul06@gmail.com>
2022-08-26 12:47:42 +0300

    feat(frontend,components): start live tracker

--- frontend/src/api/graphql/mutation/updateTrackerById.ts
+++ frontend/src/api/graphql/mutation/updateTrackerById.ts
@@ -4,6 +4,7 @@ export const UPDATE_TRACKER_BY_ID_MUTATION = gql`
   mutation updateTrackerById($id: ID!, $data: TrackerInput!) {
     updateTracker(id: $id, data: $data) {
       data {
+        id
         attributes {
           duration
           live
