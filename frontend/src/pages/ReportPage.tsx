949bcf9955b3110af66f4ff64a904f678aebf3e3
AirFast <airfast.88@gmail.com>
2022-08-30 16:43:11 +0300

    feat: add table for report data

--- frontend/src/pages/ReportPage.tsx
+++ frontend/src/pages/ReportPage.tsx
@@ -1,14 +1,69 @@
 import React from 'react';
-import { Typography } from '@mui/material';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
+} from '@mui/material';
 
 import { MainWrapper } from '../components';
 import { PageProps } from './types';
+import { Stack } from '@mui/system';
+import { useAuthUser, useNormalizedTrackers } from 'hooks';
+
+const reportTableHead = ['Date', 'Description', 'Time'];
 
 const ReportPage: React.FC<PageProps> = ({ title }) => {
+  const { user } = useAuthUser();
+  const { trackers } = useNormalizedTrackers(
+    user.id,
+    '2022-08-01',
+    '2022-08-31'
+  );
+
   return (
-    <MainWrapper>
+    <MainWrapper sidebar={<p>Filters</p>}>
       <Typography variant="h1">{title}</Typography>
-      <p>Full width container</p>
+      <Stack mt={6}>
+        <TableContainer>
+          <Table>
+            <TableHead>
+              <TableRow>
+                {reportTableHead.map((item, i) => (
+                  <TableCell key={i}>{item}</TableCell>
+                ))}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {trackers.map(({ date, trackersByProject }) =>
+                trackersByProject.map(({ name, trackers }) =>
+                  trackers.map(({ id, attributes }) => (
+                    <TableRow
+                      key={id}
+                      sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                    >
+                      <TableCell component="th" scope="row">
+                        {date}
+                      </TableCell>
+                      <TableCell>
+                        <Typography variant="h6">
+                          {name}
+                          {trackersByProject.length}
+                        </Typography>
+                        <Typography>{attributes?.description}</Typography>
+                      </TableCell>
+                      <TableCell>{attributes?.duration}</TableCell>
+                    </TableRow>
+                  ))
+                )
+              )}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Stack>
     </MainWrapper>
   );
 };
