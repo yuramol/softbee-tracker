c59aff5a7ccc136dc9f81164635897976d5c0a47
AirFast <airfast.88@gmail.com>
2022-08-31 14:55:47 +0300

    feat: add period and calculate total time for report

--- frontend/src/pages/ReportPage.tsx
+++ frontend/src/pages/ReportPage.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useMemo, useState } from 'react';
+import { format } from 'date-fns';
 import {
+  Stack,
   Table,
   TableBody,
   TableCell,
@@ -11,58 +13,109 @@ import {
 
 import { MainWrapper } from '../components';
 import { PageProps } from './types';
-import { Stack } from '@mui/system';
 import { useAuthUser, useNormalizedTrackers } from 'hooks';
+import { getHours, getMinutes, parseTrackerTime } from 'helpers';
 
 const reportTableHead = ['Date', 'Description', 'Time'];
 
 const ReportPage: React.FC<PageProps> = ({ title }) => {
   const { user } = useAuthUser();
-  const { trackers } = useNormalizedTrackers(
-    user.id,
-    '2022-08-01',
-    '2022-08-31'
+  const [startDate, setStartDate] = useState(
+    format(new Date('2022-08-01'), 'YYY-MM-dd')
   );
+  const [endDate, setEndDate] = useState(
+    format(new Date('2022-08-31'), 'YYY-MM-dd')
+  );
+
+  const { trackers } = useNormalizedTrackers(user.id, startDate, endDate);
+
+  const reportTotalTime = useMemo(() => {
+    let totalTime = '00:00';
+
+    trackers.forEach(({ total }) => {
+      totalTime = getHours(
+        getMinutes(totalTime, 'HH:mm') + getMinutes(total, 'HH:mm')
+      );
+    });
+
+    return totalTime;
+  }, [trackers]);
 
   return (
     <MainWrapper sidebar={<p>Filters</p>}>
       <Typography variant="h1">{title}</Typography>
+      <Stack mt={6} flexDirection="row" justifyContent="space-between">
+        <Stack flexDirection="row" gap={2}>
+          <Typography fontWeight="600">Period:</Typography>
+          <Typography>
+            {format(new Date(startDate), 'd MMM y')} -{' '}
+            {format(new Date(endDate), 'd MMM y')}
+          </Typography>
+        </Stack>
+        <Stack flexDirection="row" gap={2}>
+          <Typography fontWeight="600">Total:</Typography>
+          <Typography>{reportTotalTime}</Typography>
+        </Stack>
+      </Stack>
       <Stack mt={6}>
-        <TableContainer>
-          <Table>
-            <TableHead>
-              <TableRow>
-                {reportTableHead.map((item, i) => (
-                  <TableCell key={i}>{item}</TableCell>
-                ))}
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {trackers.map(({ date, trackersByProject }) =>
-                trackersByProject.map(({ name, trackers }) =>
-                  trackers.map(({ id, attributes }) => (
-                    <TableRow
-                      key={id}
-                      sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                    >
-                      <TableCell component="th" scope="row">
-                        {date}
-                      </TableCell>
-                      <TableCell>
-                        <Typography variant="h6">
-                          {name}
-                          {trackersByProject.length}
-                        </Typography>
-                        <Typography>{attributes?.description}</Typography>
-                      </TableCell>
-                      <TableCell>{attributes?.duration}</TableCell>
-                    </TableRow>
-                  ))
-                )
-              )}
-            </TableBody>
-          </Table>
-        </TableContainer>
+        {trackers.length > 0 ? (
+          <TableContainer>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  {reportTableHead.map((item, i) => (
+                    <TableCell key={i}>{item}</TableCell>
+                  ))}
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {trackers.map(({ date, trackersByProject }) =>
+                  trackersByProject.map(({ name, trackers }) =>
+                    trackers.map(({ id, attributes }, trackerIndex) => (
+                      <TableRow
+                        key={id}
+                        sx={{
+                          '&:last-child td, &:last-child th': { border: 0 },
+                        }}
+                      >
+                        {trackerIndex === 0 && (
+                          <TableCell
+                            component="th"
+                            scope="row"
+                            rowSpan={trackers.length}
+                          >
+                            {format(new Date(date), 'd MMM y')}
+                          </TableCell>
+                        )}
+
+                        <TableCell>
+                          <Typography
+                            variant="subtitle1"
+                            sx={{ fontWeight: 600 }}
+                          >
+                            {name}
+                          </Typography>
+                          <Typography>{attributes?.description}</Typography>
+                        </TableCell>
+
+                        <TableCell>
+                          {format(
+                            parseTrackerTime(attributes?.duration),
+                            'HH:mm'
+                          )}
+                        </TableCell>
+                      </TableRow>
+                    ))
+                  )
+                )}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        ) : (
+          <Typography variant="h6">
+            There is no data to generate a report
+          </Typography>
+        )}
       </Stack>
     </MainWrapper>
   );
