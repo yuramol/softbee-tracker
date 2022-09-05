726ece8ffcd2c629e2aefee7e1ec5244e4de5f2e
AirFast <airfast.88@gmail.com>
2022-09-05 10:19:02 +0300

    refactor: remove projectsList query

--- frontend/src/api/graphql/query/index.ts
+++ frontend/src/api/graphql/query/index.ts
@@ -4,4 +4,3 @@ export { USERS_QUERY } from './users';
 export { PROJECTS_QUERY } from './projects';
 export { USERS_PERMISSIONS_USER } from './usersPermissionsUser';
 export { PROJECTS_TRACKERS_BY_USER_ID_QUERY } from './projectsTrackersByUserId';
-export { PROJECTS_LIST_QUERY } from './projectsList';
