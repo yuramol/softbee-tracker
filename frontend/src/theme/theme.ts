2df29a8e92fe8b81d4f46d19d93b4ef4fbc53474
vitalekp <80340001+vitalekp@users.noreply.github.com>
2022-09-07 14:47:42 +0300

    feat: edit  typography font-size (#77)

--- frontend/src/theme/theme.ts
+++ frontend/src/theme/theme.ts
@@ -51,27 +51,27 @@ export const theme: Theme = createTheme({
   typography: {
     fontFamily: ['Poppins', 'sans-serif'].join(','),
     h1: {
-      fontSize: '2.566rem',
-      fontWeight: 600,
-    },
-    h2: {
-      fontSize: '2.281rem',
-      fontWeight: 600,
-    },
-    h3: {
       fontSize: '2.027rem',
       fontWeight: 600,
     },
-    h4: {
+    h2: {
       fontSize: '1.802rem',
       fontWeight: 600,
     },
-    h5: {
+    h3: {
       fontSize: '1.602rem',
       fontWeight: 600,
     },
-    h6: {
+    h4: {
       fontSize: '1.424rem',
+      fontWeight: 600,
+    },
+    h5: {
+      fontSize: '1.266rem',
+      fontWeight: 600,
+    },
+    h6: {
+      fontSize: '1.125rem',
       fontWeight: 600,
     },
   },
