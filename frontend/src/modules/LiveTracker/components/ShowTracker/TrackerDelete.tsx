d947e4830edc09c7634c3c9ab91961755ee7bf72
Mikhail Yastremsky <maukul06@gmail.com>
2022-09-07 22:37:34 +0300

    feat(frontend,live-tracker): add confirmation dialog for delete

--- frontend/src/modules/LiveTracker/components/ShowTracker/TrackerDelete.tsx
+++ frontend/src/modules/LiveTracker/components/ShowTracker/TrackerDelete.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ClearIcon from '@mui/icons-material/Clear';
 import { useSnackbar } from 'notistack';
 import { GraphQLError } from 'graphql';
 
 import { TrackerEntity } from 'types/GraphqlTypes';
+import { ConfirmationDialog } from 'components';
+
 import { useDeleteTracker } from '../../hooks';
 import { IconButtonTracker } from '../../helpers';
 
@@ -14,9 +16,11 @@ type TrackerDeleteProps = {
 export const TrackerDelete = ({ tracker }: TrackerDeleteProps) => {
   const { enqueueSnackbar } = useSnackbar();
   const { deleteTracker } = useDeleteTracker();
-
+  const [confirmationDialogOpen, setConfirmationDialogOpen] = useState(false);
   if (!tracker.attributes) return null;
 
+  const handelOpenConfirmationDialog = () => setConfirmationDialogOpen(true);
+  const handelCloseConfirmationDialog = () => setConfirmationDialogOpen(false);
   const handelDeleteTracker = () => {
     deleteTracker(tracker)
       .then(() => {
@@ -24,11 +28,25 @@ export const TrackerDelete = ({ tracker }: TrackerDeleteProps) => {
       })
       .catch((error: GraphQLError) => {
         enqueueSnackbar(error.message, { variant: 'error' });
+      })
+      .finally(() => {
+        handelCloseConfirmationDialog();
       });
   };
   return (
-    <IconButtonTracker onClick={handelDeleteTracker}>
-      <ClearIcon color="error" />
-    </IconButtonTracker>
+    <>
+      <ConfirmationDialog
+        open={confirmationDialogOpen}
+        onClose={handelCloseConfirmationDialog}
+        onConfirmation={handelDeleteTracker}
+        text="Are you sure you want to delete this time entry?"
+        title="Delete confirmation"
+        buttonCloseTitle="Cancel"
+        buttonConfirmationTitle="Delete"
+      />
+      <IconButtonTracker onClick={handelOpenConfirmationDialog}>
+        <ClearIcon color="error" />
+      </IconButtonTracker>
+    </>
   );
 };
