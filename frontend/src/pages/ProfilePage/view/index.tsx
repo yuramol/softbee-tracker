e9f0a75b8f2f6b49b0c0746fca0ff0e7c1d609e1
AirFast <airfast.88@gmail.com>
2022-09-08 13:11:58 +0300

    fix: disable edit profile for employee

--- frontend/src/pages/ProfilePage/view/index.tsx
+++ frontend/src/pages/ProfilePage/view/index.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import { MainWrapper } from 'components';
 import { ProfileEditView } from '../ProfileEditView';
+import { useAuthUser } from 'hooks';
 
 const ProfileViewPage = () => {
+  const { isManager } = useAuthUser();
   const { userId } = useParams();
 
   return (
     <MainWrapper>
-      <ProfileEditView id={`${userId}`} enableEdit={true} />
+      <ProfileEditView id={`${userId}`} enableEdit={isManager} />
     </MainWrapper>
   );
 };
