3de72e9fd8ef8565c2923a552778df290ef9a67f
yuramoldev <yuramol86@gmail.com>
2022-09-15 18:01:16 +0300

    fix: fix quotes

--- frontend/src/api/graphql/query/reportPDF.ts
+++ frontend/src/api/graphql/query/reportPDF.ts
@@ -2,7 +2,12 @@ import { gql } from '@apollo/client';
 
 export const REPORT_PDF_QUERY = gql`
   query ReportPDF($query: String!) {
-    reportPDF @rest(path: '/report/pdf?{args.query}', method: 'GET', type: 'ReportPDFPayload') {
+    reportPDF
+      @rest(
+        path: "/report/pdf?{args.query}"
+        method: "GET"
+        type: "ReportPDFPayload"
+      ) {
       data
     }
   }
