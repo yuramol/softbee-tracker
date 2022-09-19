f51dc9abe51d9ea17205f0522a6e5936f91f1592
Stanislav Babuch <stasbs1997@gmail.com>
2022-09-19 11:50:48 +0300

    St 111 add new entry with employee (#102)
    * build(backend): update strapi
    
    * feat(backend): create custom route
    
    * build(backend): update strapi
    
    * feat(backend): generate report
    
    * feat: add report tabs
    
    * fix: chang label Users to Employee
    
    Co-authored-by: Mikhail Yastremsky <maukul06@gmail.com>
    Co-authored-by: yuramoldev <yuramol86@gmail.com>

---
+++ backend/src/api/note/services/note.ts
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * note service.
+ */
+
+import { factories } from '@strapi/strapi'; 
+
+export default factories.createCoreService('api::note.note');
---
+++ backend/src/api/project/services/project.ts
@@ -0,0 +1,10 @@
+'use strict';
+
+/**
+ * project service.
+ */
+
+import { factories } from '@strapi/strapi'; 
+
+export default factories.createCoreService('api::project.project');
+
