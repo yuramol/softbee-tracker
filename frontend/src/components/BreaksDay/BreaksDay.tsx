acf79f5a3719c38eef2d95e42a23bd6327a1ffba
Stanislav Babuch <stasbs1997@gmail.com>
2022-09-22 10:00:05 +0300

    feat:add universal getBreacksIcon component , fix login page image (#110)
    * feat:add universal getBreacksIcon component , fix login page image
    
    * refactor: name update

--- frontend/src/components/BreaksDay/BreaksDay.tsx
+++ frontend/src/components/BreaksDay/BreaksDay.tsx
@@ -1,29 +1,16 @@
 import * as React from 'react';
 
 import { Typography, Stack } from '@mui/material';
-import { Icon } from 'legos';
+import { getBreakIcon } from './getBreakIcon';
 type VacationDayProps = {
   description?: string;
   breaks?: string;
 };
 export const BreaksDay = ({ description, breaks }: VacationDayProps) => {
-  const getBreackIcon: (type?: string) => JSX.Element | null = (type) => {
-    switch (type) {
-      case 'Vacation':
-        return <Icon icon="sailing" />;
-      case 'Unpaid':
-        return <Icon icon="moneyOff" />;
-      case 'Sickness':
-        return <Icon icon="medicalServices" />;
-      default:
-        return null;
-    }
-  };
   return (
     <Stack>
       <Stack direction="row" spacing={2}>
-        {getBreackIcon(breaks)}
-
+        {getBreakIcon(breaks)}
         <Typography ml={0.5} fontWeight={600}>
           {breaks}
         </Typography>
---
+++ frontend/src/components/BreaksDay/getBreakIcon.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { Breaks } from 'constant';
+import { Icon, IconsNames } from 'legos/Icon';
+import { toUpperCaseFirst } from '../../helpers/toUpperCaseFirst';
+
+export const getBreakIcon = (value?: string) => {
+  let icon: IconsNames = 'moneyOff';
+
+  if (toUpperCaseFirst(Breaks.Vacation) === value) {
+    icon = 'sailing';
+  } else if (toUpperCaseFirst(Breaks.Sickness) === value) {
+    icon = 'medicalServices';
+  }
+  return <Icon icon={icon} />;
+};
