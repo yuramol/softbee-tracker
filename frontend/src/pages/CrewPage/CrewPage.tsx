004f2ac75e42bd531d792e35e3e726598bd8334b
Stanislav Babuch <stasbs1997@gmail.com>
2023-04-11 19:54:37 +0300

    feat:add total timer time crew page

--- frontend/src/pages/CrewPage/CrewPage.tsx
+++ frontend/src/pages/CrewPage/CrewPage.tsx
@@ -29,7 +29,7 @@ const CrewPage: React.FC<PageProps> = () => {
               onClick={onToggleForm}
             />
           )}
-          <TimeInspector />
+          <TimeInspector userId={isManager ? undefined : user.id} />
         </>
       }
     >
