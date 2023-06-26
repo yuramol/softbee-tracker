0c432972bd43ca01206b7df7af7906da224c605d
Dmytro Dukhov <49958315+clever-giraffe@users.noreply.github.com>
2023-06-26 06:33:36 +0300

    Create ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,56 @@
+module.exports = {
+  apps: [
+    {
+      name: 'dev.strapi.track.sofrtbee.io',
+      script: 'npm',
+      args: 'start',
+      cwd: './backend',
+      env: {
+        NODE_ENV: 'development',
+        PORT: 1337,
+      },
+    },
+    {
+      name: 'dev.track.sofrtbee.io',
+      script: 'npm',
+      args: 'start',
+      cwd: './frontend-shop',
+      env: {
+        NODE_ENV: 'development',
+        PORT: 8081,
+      },
+    },
+    {
+      name: 'front',
+      script: 'serve',
+      cwd: './frontend',
+      env: {
+        NODE_ENV: 'development',
+        PORT: 8081, 
+        PM2_SERVE_PATH: './build',
+        PM2_SERVE_SPA: 'true',
+        PM2_SERVE_PORT: 8085
+      },
+    },
+    {
+      name: 'strapi.track.sofrtbee.io',
+      script: 'npm',
+      args: 'start',
+      cwd: './backend',
+      env: {
+        NODE_ENV: 'production',
+        PORT: 1437,
+      },
+    },
+    {
+      name: 'track.sofrtbee.io',
+      script: 'npm',
+      args: 'start',
+      cwd: './frontend',
+      env: {
+        NODE_ENV: 'production',
+        PORT: 8109,
+      },
+    },
+  ],
+};
