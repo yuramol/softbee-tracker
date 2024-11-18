c98fae589f54f7c05da7fbfedf9f21e3941bb29d
vitalekp <80340001+vitalekp@users.noreply.github.com>
2024-11-18 20:33:04 +0200

    Crew profile sidebar (#214)

--- frontend/src/components/VacationWidget/VacationWidget.tsx
+++ frontend/src/components/VacationWidget/VacationWidget.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { FC, useEffect } from 'react';
 import { endOfYear, format, startOfYear } from 'date-fns';
 import { List, ListItem, ListItemText, Stack, Typography } from '@mui/material';
 
@@ -7,15 +7,23 @@ import { Breaks } from 'constant';
 import { BreaksRequest } from 'components/BreaksRequest';
 import { useAuthUser, useNormalizedTrackers } from 'hooks';
 
-export const VacationWidget = () => {
+interface Props {
+  userId?: string;
+  isCrewProfile?: boolean;
+}
+
+export const VacationWidget: FC<Props> = ({ userId, isCrewProfile }) => {
   const { user } = useAuthUser();
 
   const startYear = format(startOfYear(new Date()), 'YYY-MM-dd');
   const endYear = format(endOfYear(new Date()), 'YYY-MM-dd');
   const vacationProjects = ['14', '16', '15'];
+
+  const currentUserId = isCrewProfile && userId ? userId : user.id;
+
   const { normalizedTrackers, fetchTrackers } = useNormalizedTrackers(
     {
-      user: { id: { in: [user.id] } },
+      user: { id: { in: [currentUserId] } },
       date: { between: [startYear, endYear] },
       project: {
         id: { in: vacationProjects },
@@ -28,7 +36,7 @@ export const VacationWidget = () => {
     fetchTrackers({
       variables: {
         filters: {
-          user: { id: { in: [user.id] } },
+          user: { id: { in: [currentUserId] } },
           date: { between: [startYear, endYear] },
           project: {
             id: { in: vacationProjects },
@@ -53,9 +61,11 @@ export const VacationWidget = () => {
   return (
     <>
       <Stack gap={3} mb={3}>
-        <Stack gap={1}>
-          <BreaksRequest />
-        </Stack>
+        {(userId && isCrewProfile && userId === user.id) || !isCrewProfile ? (
+          <Stack gap={1}>
+            <BreaksRequest />
+          </Stack>
+        ) : null}
 
         <List disablePadding>
           <ListItem disableGutters disablePadding>
