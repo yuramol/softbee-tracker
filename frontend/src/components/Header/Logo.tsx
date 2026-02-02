3828833f4d3e38a81ec2dd318950d4406d478ccc
yuramol <yuramol86@gmail.com>
2026-02-02 11:52:45 +0500

    Update logo

--- frontend/src/components/Header/Logo.tsx
+++ frontend/src/components/Header/Logo.tsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 
-import LogoMain from "../../assets/SoftBee_Logo.png";
+import LogoMain from "../../assets/SoftBee_Logo.webp";
 
 export const Logo = () => (
   <Link to="/">
